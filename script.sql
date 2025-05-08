82f3af808fba443a6d994da6f86af2a37138755a
Gabriel <gabrielcorolleite@gmail.com>
2025-05-08 15:40:19 -0300

    Criando o script do banco de dados e instalando o MySQL.Data

---
+++ script.sql
@@ -0,0 +1,17 @@
+create database dbProjeto;
+
+use dbProjeto;
+
+create table Usuario (Id int primary key,
+Nome varchar(50),
+Email varchar(50),
+Senha varchar(50));
+
+create table Produto (Id int primary key,
+Nome varchar(50),
+Descricao varchar(200),
+Preco decimal,
+Quantidade varchar(50));
+
+select * from Usuario;
+select * from Produto;
