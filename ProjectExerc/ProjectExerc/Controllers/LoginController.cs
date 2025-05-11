c57df284af694d9fc8f6030b7300ce2384a81c54
Gabriel <gabrielcorolleite@gmail.com>
2025-05-11 13:56:49 -0300

    Criando e desenvolvendo a LoginController

---
+++ ProjectExerc/ProjectExerc/Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectExerc.Models;
+using ProjectExerc.Repositorio;
+
+namespace ProjectExerc.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly LoginRepositorio _loginRepositorio;
+        public LoginController(LoginRepositorio loginRepositorio)
+        {
+            _loginRepositorio = loginRepositorio;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string email, string senha)
+        {
+            var login = _loginRepositorio.ObterUsuario(email);
+            if(login != null && login.Senha == senha)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            ModelState.AddModelError("", "Email ou senha incorretos!");
+            return View();
+        }
+    }
+}
