d2a4e7e45e9d9ac28631c936508636922a83b5fd
Gabriel <gabrielcorolleite@gmail.com>
2025-05-14 18:55:45 -0300

    Criando e desenvolvendo a view Cadastrar Produto

--- ProjectExerc/ProjectExerc/Program.cs
+++ ProjectExerc/ProjectExerc/Program.cs
@@ -1,9 +1,15 @@
+using ProjectExerc.Repositorio;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddScoped<ProdutoRepositorio>();
+builder.Services.AddScoped<LoginRepositorio>();
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
--- script.sql
+++ script.sql
@@ -2,12 +2,12 @@ create database dbProjeto;
 
 use dbProjeto;
 
-create table Usuario (Id int primary key,
+create table Usuario (Id int primary key AUTO_INCREMENT,
 Nome varchar(50),
 Email varchar(50),
 Senha varchar(50));
 
-create table Produto (Id int primary key,
+create table Produto (Id int primary key AUTO_INCREMENT,
 Nome varchar(50),
 Descricao varchar(200),
 Preco decimal,
